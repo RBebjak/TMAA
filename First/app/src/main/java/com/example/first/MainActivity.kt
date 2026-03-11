917b8966fcbb483bf643584a82a49525361c2b87
RBebjak <richard.bebjak01@gmail.com>
2026-03-11 13:56:19 +0100

    maps

--- First/app/src/main/java/com/example/first/MainActivity.kt
+++ First/app/src/main/java/com/example/first/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.example.first
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -19,6 +23,7 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -26,6 +31,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
@@ -33,6 +40,9 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.first.ui.theme.FirstTheme
 import kotlinx.serialization.Serializable
+import com.google.maps.android.compose.*
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
 
 class MainActivity : ComponentActivity() {
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -73,16 +83,15 @@ fun MyAppNavigation() {
         composable<FirstScreenRoute> { backStackEntry ->
             val userId = backStackEntry.arguments?.getString("userId") ?: ""
             FirstScreen(userId = userId, { userId ->
+
                 navController.navigate(SecondScreenRoute(userId))
             },onLogout = {navController.navigate(LoginScreenRoute)})
         }
 
-        composable<SecondScreenRoute> { backStackEntry ->
-            val userId = backStackEntry.arguments?.getString("userId") ?: ""
-            SecondScreen(userId, onNavigate = {
-                    userId ->
-                navController.navigate(FirstScreenRoute(userId))
-            })
+        composable<SecondScreenRoute> {
+            SecondScreen(
+                onBack = { navController.popBackStack() }
+            )
         }
     }
 }
@@ -99,7 +108,7 @@ fun LoginScreen(onNavigate: (String) -> Unit) {
         verticalArrangement = Arrangement.Center
     ) {
         Text(
-            text = "Login",
+            text = stringResource(R.string.app_name) ,
             style = MaterialTheme.typography.headlineMedium
         )
 
@@ -165,48 +174,52 @@ fun FirstScreen(userId: String, onNavigate: (String) -> Unit, onLogout: () -> Un
     }
 }
 @Composable
-fun SecondScreen(userId: String, onNavigate: (String) -> Unit) {
-    var textInput by remember { mutableStateOf("") }
+fun SecondScreen(onBack: () -> Unit) {
+
+    val context = LocalContext.current
+
+    val permissionLauncher =
+        rememberLauncherForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { }
+
+    LaunchedEffect(Unit) {
+        permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+
+    val location = LatLng(49.5938, 17.2509)
+
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(location, 12f)
+    }
 
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        modifier = Modifier.fillMaxSize()
     ) {
-        Text(
-            text = "Meno:",
-            style = MaterialTheme.typography.displayMedium
-        )
 
-        Text(
-            text = userId,
-            style = MaterialTheme.typography.displaySmall
-        )
+        GoogleMap(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth(),
+            cameraPositionState = cameraPositionState,
+            properties = MapProperties(
+                isMyLocationEnabled = true
+            )
+        ) {
 
-        Text(
-            text = "Nove meno:",
-            style = MaterialTheme.typography.displayMedium
-        )
-
-        OutlinedTextField(
-            value = textInput,
-            onValueChange = { textInput = it },
-            label = { Text("Zadejte User ID") },
-            singleLine = true
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
+            Marker(
+                state = MarkerState(position = location),
+                title = "Example location"
+            )
+        }
 
         Button(
-            onClick = {
-                if (textInput.isNotBlank()) {
-                    onNavigate(textInput)
-                }
-            }
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            onClick = { onBack() }
         ) {
-            Text("Ulozit")
+            Text("Back")
         }
     }
 }
